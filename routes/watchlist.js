743b545cbbd3aa478caa24cfc3fa91de2e071275
Ahmed Khaled <ahmed@mrkhalednasr.com>
2022-06-20 01:43:34 +0200

    change api types to post

--- routes/watchlist.js
+++ routes/watchlist.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const watchListcontroller = require('../controller/watchlistcontroller');
 
-router.get('/get',watchListcontroller.getlist);
-router.delete('/delete',watchListcontroller.deletefromlist);
+router.post('/get',watchListcontroller.getlist);
+router.post('/delete',watchListcontroller.deletefromlist);
 router.put('/add',watchListcontroller.addtolist);
 
 module.exports = router;
